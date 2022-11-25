e6b354880972d57cc95c178d1390131bfbbe3d3e
sorenoid <sorenoid@gmail.com>
2022-11-25 15:46:29 -0800

    head tag tamplate

--- src/main/kotlin/com/bandor/plugins/Routing.kt
+++ src/main/kotlin/com/bandor/plugins/Routing.kt
@@ -11,37 +11,25 @@ import io.ktor.server.util.*
 fun Application.configureRouting() {
 
     routing {
+       // trace { application.log.trace(it.buildText()) }
         static {
             staticBasePackage = "manual-html"
             resource("robots.txt")
             resource("sitemap.xml.gz")
+            resource("css/style.css", resource = "style.css" )
+            resource("css/layout.css", resource = "layout.css" )
         }
 
         get("/") {
             call.respond(FreeMarkerContent("index.ftl", mapOf("manuals" to Html2Json.manuals)))
         }
 
-        get("{level}") {
-            try {
-                val level = call.parameters.getOrFail<Int>("level").toInt()
-                Html2Json.manuals.find { it.level == level && it.name.lowercase() == "intro" }?.let {
-                    call.respond(
-                        FreeMarkerContent(
-                            "show.ftl",
-                            mapOf("manual" to it)
-                        )
-                    )
-                } ?: call.respondRedirect("/")
-            } catch (e: Exception) {
-                call.respondRedirect("/")
-            }
-
-        }
-
         get("{level}/{name}") {
             try {
+
                 val level = call.parameters.getOrFail<Int>("level").toInt()
                 val name = call.parameters.getOrFail<String>("name").toString()
+                application.log.debug("GOT $level and $name")
                 Html2Json.manuals.find { it.level == level && it.name.lowercase() == name.lowercase() }?.let {
                     call.respond(
                         FreeMarkerContent(
@@ -51,9 +39,45 @@ fun Application.configureRouting() {
                     )
                 } ?: call.respondRedirect("/")
             } catch (e: Exception) {
+                application.log.debug("GOT exception ")
+                e.printStackTrace()
                 call.respondRedirect("/")
             }
         }
+
+        get("{level}") {
+            try {
+                val param = call.parameters.getOrFail("level")
+                application.log.debug("GOT $param")
+                param.toIntOrNull()?.let { level ->
+                    Html2Json.manuals.find { it.level == level && it.name.lowercase() == "intro" }?.let {
+                        call.respond(
+                            FreeMarkerContent(
+                                "show.ftl",
+                                mapOf("manual" to it)
+                            )
+                        )
+                    } ?: call.respondRedirect("/")
+                } ?: run {
+                    application.log.debug("GOT $param as name")
+                    Html2Json.manuals.find { it.name.lowercase() == param.lowercase() }?.let {
+                        call.respond(
+                            FreeMarkerContent(
+                                "show.ftl",
+                                mapOf("manual" to it)
+                            )
+                        )
+                    } ?: call.respondRedirect("/")
+
+                }
+
+            } catch (e: Exception) {
+                application.log.debug("GOT ${e.stackTrace}")
+                call.respondRedirect("/")
+            }
+
+        }
+
     }
 }
 
